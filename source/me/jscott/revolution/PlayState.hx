ad5fed54e75172f21fe59223f2a7ea7beec7ce46
Jonathan Scott <jonathan@jscott.me>
2017-02-11 04:17:29 +0000

    add day text

--- source/me/jscott/revolution/PlayState.hx
+++ source/me/jscott/revolution/PlayState.hx
@@ -24,10 +24,16 @@ class PlayState extends FlxState {
     var startDate = DateTime.fromString('1670-01-01 00:00:01');
 
     // Interface elements
+    var dayText:FlxText;
     var timeText:FlxText;
 
     private function createUI():Void {
-        timeText = new FlxText(FlxG.width - 170, 10, 160); // x, y, width
+        dayText = new FlxText(FlxG.width - 170, 10, 160); // x, y, width
+        dayText.setFormat(null, 20, FlxColor.WHITE, CENTER);
+        dayText.text = formatDayText(time);
+        add(dayText);
+
+        timeText = new FlxText(FlxG.width - 170, dayText.y + dayText.height, 160); // x, y, width
         timeText.setFormat(null, 20, FlxColor.WHITE, CENTER);
         timeText.text = formatTimeText(time);
         add(timeText);
@@ -40,6 +46,33 @@ class PlayState extends FlxState {
         // TODO turn time into a date string
         var date = startDate + Day(time);
         return date.format("%d/%m/%Y");
+    }
+
+    private function formatDayText(time:Int) {
+        var date = startDate + Day(time);
+        var dow = date.format("%w");
+        if (dow == "0") {
+            return "SUN";
+        }
+        if (dow == "1") {
+            return "MON";
+        }
+        if (dow == "2") {
+            return "TUE";
+        }
+        if (dow == "3") {
+            return "WED";
+        }
+        if (dow == "4") {
+            return "THU";
+        }
+        if (dow == "5") {
+            return "FRI";
+        }
+        if (dow == "6") {
+            return "SAT";
+        }
+        return "???";
     }
 
     public function new(game:Game): Void {
@@ -70,6 +103,7 @@ class PlayState extends FlxState {
         time += 1;
 
         // TODO: Refresh time indicator
+        dayText.text = formatDayText(time);
         timeText.text = formatTimeText(time);
     }
 }
