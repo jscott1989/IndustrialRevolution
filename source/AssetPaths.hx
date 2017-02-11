907348851866058a4be5d661de3567832d73f0ea
Jonathan Scott <jonathan@jscott.me>
2017-02-11 05:22:47 +0000

    Remove all haxe source - moving to HTML/JS

--- source/AssetPaths.hx
+++
@@ -1,7 +0,0 @@
-package;
-
-/**
-* Assets get attached to this class so they can be referenced throughout the game.
-**/
-@:build(flixel.system.FlxAssets.buildFileReferences("assets", true))
-class AssetPaths {}
--- source/me/jscott/revolution/data/game/Technology.hx
+++
@@ -1,7 +0,0 @@
-package me.jscott.revolution.data.game;
-
-class Technology {
-    public function new() {
-
-    }
-}
